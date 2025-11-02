8fc95e80e62d826b691042a4adf19a10bd51327c
Edison <edisonngunjiri@gmail.com>
2025-11-02 15:09:14 +0300

    renamed to REACT_APP_API_BASE_URL

--- src/utils/Http.js
+++ src/utils/Http.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const apiClient = axios.create({
-  baseURL: process.env.REACT_APP_API_LOCAL_BASE_URL
+  baseURL: process.env.REACT_APP_API_BASE_URL
 });
 
 // Add a request interceptor to include the token
